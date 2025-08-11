29a072a7245acded20478ea6d338c70d8ab0cf5a
chinnari-2002 <chinnari.a@egradtutor.in>
2025-08-11 19:43:33 +0530

    remove the ai ml

--- ndmatrix/src/app/components/verticals-page/verticals-page.ts
+++ ndmatrix/src/app/components/verticals-page/verticals-page.ts
@@ -10,8 +10,8 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
   styleUrls: ['./verticals-page.css']
 })
 export class VerticalsPage {
-  constructor(private router:Router , private activatedRoute:ActivatedRoute){
-    this.currentRoute=this.router.url
+  constructor(){
+   
   }
    hoverCard: any = null;
 currentRoute: string = '';
@@ -21,21 +21,20 @@ currentRoute: string = '';
       description:`nDMatrix provides advanced tools and learning resources for  students, educators, and professionals aiming for industry readiness.`,
       verticalImg:"images/aerospace.jpg",
     },
-    {
-    title:"Robotics/Mechatronics",
-    description:`We offers hands-on learning tools and real-world project support in Robotics and Mechatronics,We empower students to design, simulate skills.`,
- verticalImg:"images/robotic.jpg",
-    },
-     {
-    title:"AI/ML Software Tools",
-    description:`We provides AI/ML tools and learning modules tailored for practical implementation and academic growth.`,
- verticalImg:"images/mlalogorithm.jpg",
-    },
+//     {
+//     title:"Robotics/Mechatronics",
+//     description:`We offers hands-on learning tools and real-world project support in Robotics and Mechatronics,We empower students to design, simulate skills.`,
+//  verticalImg:"images/robotic.jpg",
+//     },
+//      {
+//     title:"AI/ML Software Tools",
+//     description:`We provides AI/ML tools and learning modules tailored for practical implementation and academic growth.`,
+//  verticalImg:"images/mlalogorithm.jpg",
+//     },
      {
     title:"Software Development Center",
     description:` We supports students and educators in building real-world applications we offer guided, project-based learning experiences.`,
  verticalImg:"images/softwareDev.jpg",
- route:"/software-development-center/products"
     }
   ]
 
@@ -46,15 +45,15 @@ currentRoute: string = '';
 onMouseLeave() {
   this.hoverCard = null;
 }
- // Check if this card's route matches current URL
-  isActive(card: any): boolean {
-    return card.route === this.currentRoute;
-  }
-onCardClick(card: any, event: MouseEvent) {
-    if (card.route) {
-      event.preventDefault(); 
-      this.router.navigate([card.route]);
-    }
-  }
+//  // Check if this card's route matches current URL
+//   isActive(card: any): boolean {
+//     return card.route === this.currentRoute;
+//   }
+// onCardClick(card: any, event: MouseEvent) {
+//     if (card.route) {
+//       event.preventDefault(); 
+//       this.router.navigate([card.route]);
+//     }
+//   }
 
 }
